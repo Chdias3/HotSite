1bc6e0a595ee9835af26fce26047dfbfdb03c245
Charles Alexsander <chdias30@outlook.com>
2022-06-29 12:50:42 -0300

    :tada: first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Hotsite.Models;
+
+namespace Hotsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Interesse cad)
+        {
+            try
+            {
+            DatabaseService dbs = new DatabaseService();
+            dbs.CadastraInteresse(cad);
+           ViewData["Mensagem"] = "Cadastro efetuado com sucesso";
+            }
+            catch (Exception e)
+            {  
+               _logger.LogError("Erro ao gravar no banco " + e.Message);
+               ViewData["Mensagem"] = "Erro ao cadastrar. Tente mais tarde. ";
+            }
+          return View("Index",cad); 
+        }
+
+    }
+}
